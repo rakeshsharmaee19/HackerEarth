a0765bf44e27cc314ae359a98d18ff27158092f9
rakeshsharmaee19 <91816394+rakeshsharmaee19@users.noreply.github.com>
2023-07-04 11:31:00 +0530

    Update gift_for_almas.py

--- Data Structure/arrays/multi-dimensional/gift_for_almas.py
+++ Data Structure/arrays/multi-dimensional/gift_for_almas.py
@@ -1,3 +1,4 @@
+# Solution 1
 import numpy as np
 mat = []
 for i in range(int(input())):
@@ -13,5 +14,31 @@ for i in rotation:
         m = np.array(mat, int)
         new=np.rot90(m, k=1, axes=(0,1))
         mat = new.tolist()
+for i in mat:
+    print(' '.join(str(j) for j in i))
+
+#Solution 2
+import copy
+n = int(input())
+mat = []
+res_mat = copy.deepcopy(mat)
+
+for i in range(n):
+    mat.append(list(map(int, input().split())))
+rotation = input()
+res_mat = copy.deepcopy(mat)
+
+for i in rotation:
+    if i=='R':
+        for j in range(n):
+            for k in range(n):
+                res_mat[k][n-j-1] = mat[j][k]
+    else:
+        for j in range(n):
+            for k in range(n):
+                res_mat[n-k-1][j] = mat[j][k]
+    mat = res_mat
+    res_mat=copy.deepcopy(mat)
+
 for i in mat:
     print(' '.join(str(j) for j in i))
