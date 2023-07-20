68e8bbc0dd946d1bfa3e429eaf6eafb96b03c8fb
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-07-20 14:13:48 +0530

    Update square_transaction.py

--- Algorithms/searching/linear_search/square_transaction.py
+++ Algorithms/searching/linear_search/square_transaction.py
@@ -1,21 +1,31 @@
-import numpy as np
+import bisect
 n = input()
-arr = list(map(int, input().split()))
+total = 0
+arr = []
+for each in input().split():
+    total+=int(each)
+    arr.append(total)
 
 for _ in range(int(input())):
     q = int(input())
-    itr = iter(arr)
-    ch = True
-    sm= 0
-    count = 0
-    while ch:
-        try:
-            sm+=next(itr)
-            count+=1
-            if sm>=q:
-                ch=False
-        except:
-            count = -1
-            ch =False
-    print(count)
+
+    print(-1) if q>arr[len(arr)-1] else print(bisect.bisect_left(arr,q)+1)
             
+
+
+# for _ in range(int(input())):
+#     q = int(input())
+#     itr = iter(arr)
+#     ch = True
+#     sm= 0
+#     count = 0
+#     while ch:
+#         try:
+#             sm+=next(itr)
+#             count+=1
+#             if sm>=q:
+#                 ch=False
+#         except:
+#             count = -1
+#             ch =False
+#     print(count)
