392e64661fea8a6592ed9b5f83525f45ab348f1a
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-07-21 11:45:11 +0530

    Update monk_and_power_of_time.py

--- Data Structure/arrays/1-D array/monk_and_power_of_time.py
+++ Data Structure/arrays/1-D array/monk_and_power_of_time.py
@@ -1,13 +1,13 @@
 n = int(input())
 s = 0
-c_o = list(map(int,input().split()))
-p_o = list(map(int,input().split()))
+co = list(map(int,input().split()))
+po = list(map(int,input().split()))
 
 while p_o:
-    while(c_o[0] != p_o[0]):
-        c_o = c_o[1:]+c_o[:1]
+    while(co[0] != po[0]):
+        co = co[1:]+co[:1]
         s = s + 1
-    p_o.pop(0)
-    c_o.pop(0)
+    po.pop(0)
+    co.pop(0)
     s = s+1
 print(s)
