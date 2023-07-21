54c3749571d8d287244213f2ad49ebd51a7ff620
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-07-21 11:44:00 +0530

    Create monk_and_power_of_time.py

---
+++ Data Structure/arrays/1-D array/monk_and_power_of_time.py
@@ -0,0 +1,12 @@
+n = int(input())
+s = 0
+c_o = list(map(int,input().split()))
+p_o = list(map(int,input().split()))
+while p_o:
+    while(c_o[0] != p_o[0]):
+        c_o = c_o[1:]+c_o[:1]
+        s = s + 1
+    p_o.pop(0)
+    c_o.pop(0)
+    s = s+1
+print(s)
